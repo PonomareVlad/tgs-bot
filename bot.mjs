cb9a26590dd72d3171ce776caf725f5ff1093b87
PonomareVlad <Git@PonomareVlad.ru>
2022-12-07 03:23:16 +0500

    MIME check

--- bot.mjs
+++ bot.mjs
@@ -1,25 +1,36 @@
+import {basename, extname} from "path";
 import {lookup} from "mime-types";
 import FormData from 'form-data';
 import TeleBot from "telebot";
 import https from "https";
 
-const url = 'https://tgs-vercel.vercel.app/api/'
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const {API_URL, TELEGRAM_BOT_TOKEN} = process.env;
+const bot = new TeleBot(TELEGRAM_BOT_TOKEN);
+const getFileName = (name = 'sticker.tgs') => basename(name, extname(name)) + '.tgs';
 
 bot.on('text', msg => msg.reply.text(`Send me SVG file`))
 bot.on('document', async msg => {
     let {document: {mime_type, file_name, file_id}} = msg;
     if (!mime_type && file_name) mime_type = lookup(file_name);
+    if (mime_type !== 'image/svg+xml')
+        return msg.reply.text(`Only SVG files are supported, your file has an unsupported MIME: ${mime_type}`, {asReply: true});
     const {fileLink} = await bot.getFile(file_id);
-    const form = new FormData();
-    return new Promise((resolve) => https.request(fileLink, response => {
+    const sticker = await convert(fileLink);
+    const fileName = getFileName(file_name);
+    const {message_id: replyToMessage} = await msg.reply.file(sticker, {fileName, asReply: true})
+    return msg.reply.text('Now you can forward this sticker to @Stickers bot', {replyToMessage})
+})
+
+function convert(url) {
+    return new Promise(resolve => https.request(url, response => {
+        const form = new FormData();
         form.append('file', response);
-        form.submit(url, function (err, res) {
+        form.submit(API_URL, (err, res) => {
             const chunks = [];
             res.on('data', chunks.push.bind(chunks));
-            res.on("end", () => resolve(msg.reply.file(Buffer.concat(chunks), {fileName: 'sticker.tgs'})));
+            res.on("end", () => resolve(Buffer.concat(chunks)));
         });
     }).end());
-})
+}
 
 export default bot
